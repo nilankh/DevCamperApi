2ffdb54f0feddfb242be0842d378aeca0f987e9d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-26 23:08:24 +0530

    created controller folder and dummy functionality inside it to just check

---
+++ controllers/bootcamps.js
@@ -0,0 +1,37 @@
+// @desc    Get all bootcamps
+// @route   Get /api/v1/bootcamps
+// @access  Public
+module.exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'Show all bootcamps'});
+}
+
+
+// @desc    Get single bootcamps
+// @route   Get /api/v1/bootcamps/:id
+// @access  Public
+module.exports.getBootCamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Show bootcamps ${req.params.id}`});
+}
+
+
+// @desc    Create new bootcamps
+// @route   POST /api/v1/bootcamps
+// @access  Private
+module.exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: 'Create new bootcamp'});
+}
+
+
+// @desc    Update bootcamps
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+module.exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Update bootcamps ${req.params.id}`});
+}
+
+// @desc    Delete bootcamps
+// @route   Delete /api/v1/bootcamps/:id
+// @access  Private
+module.exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Delete bootcamps ${req.params.id}`});
+}
