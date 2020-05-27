622b4c4a680d63dd800f8719f24e2f8a9799670b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-27 16:28:34 +0530

    learned about how to create 3rd party middleware

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+// @desc Logs request to console
+const logger = (req, res, next) => {
+    // req.hello = 'Hello World';
+    // console.log('Middleware run');
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const logger = require('./middleware/logger');
 // Routes files
 const bootcamps = require('./routes/bootcamps');
 
@@ -8,6 +8,10 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+
+
+app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
