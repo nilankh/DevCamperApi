c00600ce5a0980ac7586b96e93c48f22292289e2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-27 23:01:19 +0530

    written controllers for fetching by id tnat is getbootcamp controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,6 @@ const Bootcamp = require('../models/Bootcamp');
 module.exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
-        
         res.status(200).json({ success: true,data: bootcamps});
     } catch (err) {
         res.status(400).json({success:false});
@@ -19,8 +18,18 @@ module.exports.getBootcamps = async (req, res, next) => {
 // @route   Get /api/v1/bootcamps/:id
 // @access  Public
 module.exports.getBootcamp = async (req, res, next) => {
-    
-}
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        
+        if(!bootcamp){
+            return res.status(400).json({ success: false });
+         }
+
+        res.status(200).json({ success: true, data: bootcamp })
+    } catch (err) {
+        res.status(400).json({success:false});
+    }
+};
 
 
 // @desc    Create new bootcamps
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { 
-    getBootcamp,
     getBootcamps,
+    getBootcamp,
     createBootcamp,
     updateBootcamp,
     deleteBootcamp 
