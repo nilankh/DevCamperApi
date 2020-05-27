9bbbd6d15658880844427a29c31ccc25c1a4bf6e
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-27 23:11:41 +0530

    wriiten controller for update

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -55,13 +55,21 @@ module.exports.createBootcamp = async (req, res, next) => {
 // @desc    Update bootcamps
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-module.exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamps ${req.params.id}`});
+module.exports.updateBootcamp = async (req, res, next) => {
+   
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+    });
+    if(!bootcamp){
+        return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp});
 }
 
 // @desc    Delete bootcamps
 // @route   Delete /api/v1/bootcamps/:id
 // @access  Private
-module.exports.deleteBootcamp = (req, res, next) => {
+module.exports.deleteBootcamp = async (req, res, next) => {
     res.status(200).json({ success: true, msg: `Delete bootcamps ${req.params.id}`});
 }
