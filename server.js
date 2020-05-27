3b6e2348584c5acd0b1d8c93626804c70bb86006
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-27 21:45:44 +0530

    installed colored package and setted the color for error and many many things

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env cars
@@ -25,17 +26,16 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api/v1/bootcamps', bootcamps);
 
 
-sdfsdfdsf
+
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen((PORT), () => {
-    console.log(`App listening on port ${process.env.NODE_ENV} mode on PORT ${PORT}`);
+    console.log(`App listening on port ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold);
 });
-
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     // Close server and exit process
     server.close(() => process.exit(1));
 })
