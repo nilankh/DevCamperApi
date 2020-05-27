274dd9574388f4e4347629ff5898d350482270a5
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-27 21:28:49 +0530

    connected to mongoose with the help of atlas compass

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
-// Routes files
-const bootcamps = require('./routes/bootcamps');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env cars
 dotenv.config({ path: './config/config.env' });
 
+// Connect To Database
+connectDB();
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
 
-
-app.use(logger);
+// Dev Loggin middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+// app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -21,6 +29,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen((PORT), () => {
+const server = app.listen((PORT), () => {
     console.log(`App listening on port ${process.env.NODE_ENV} mode on PORT ${PORT}`);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+})
