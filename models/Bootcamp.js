3efff8047d25eaf39540ec27989d0beb51cf4c0c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-30 22:24:20 +0530

    mongoose middleware slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -98,5 +99,10 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
       }
 });
-
+// create bootcamp slug from the name
+BootcampSchema.pre('save', function(next){
+  console.log('Slugify ran', this.name)
+  this.slug = slugify(this.name, { lower: true })
+  next();
+})
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
